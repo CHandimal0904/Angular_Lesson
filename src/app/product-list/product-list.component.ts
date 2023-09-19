4573e884ef364fea5827e1c94362e701633b67c1
Tharindu Nimantha <tharindun@sdhgroup.com>
2023-09-19 09:16:16 +0530

    ngFor Directive 2

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,30 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  // name ="saman"
-  addToCart: number = 0;
-  product = {
-  name: "Iphone 13",
-  price: 800,
-  color: "Black",
-  discount_price: 8.9,
-  stock: 3,
-  pImage: '../../assets/img/iphonex.png'
-  }
-
-  onNameChange(event:any){
-    // this.name = event.target.value;
-  }
-  decrementCartValue(){
-    if (this.addToCart>0) {
-      this.addToCart--;
-    }
-    
-  }
-  incrementCartValue(){
-    if (this.addToCart< this.product.stock) {
-      this.addToCart++;
-    }
-   
-  }
+  listofString: string[] = ['mark','steve','andrew','kamal','widow'];
 }
