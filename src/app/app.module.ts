9c7963c56206d9f185db0534f60e533cac7a9953
Tharindu Nimantha <tharindun@sdhgroup.com>
2023-09-20 16:31:23 +0530

    @input Decoretor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { containerComponent } from './container/container.component';
 import { SearchComponent } from './container/search/search.component';
 import { FormsModule } from '@angular/forms';
 import { ProductListComponent } from './container/product-list/product-list.component';
+import { ProductComponent } from './container/product-list/product/product.component';
+import { FilterComponent } from './container/product-list/filter/filter.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,8 @@ import { ProductListComponent } from './container/product-list/product-list.comp
     containerComponent,
     SearchComponent,
     ProductListComponent,
+    ProductComponent,
+    FilterComponent,
   ],
   imports: [
     BrowserModule,
